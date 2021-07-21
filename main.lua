69d8466edc95c37e1acad0da44d53fdf15e57d8d
John Winstead <brooswit@gmail.com>
2021-07-20 21:51:14 -0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -83,6 +83,7 @@ end
 
 local stateName = "main"
 local choice = nil
+local skipInput = false
 
 while true do
     config = cleanConfig(config)
@@ -105,15 +106,6 @@ while true do
         print( "1 ) Configure LaunchDarkly" )
         print( "2 ) Configure Interfaces" )
         print( "---------------------------------------------------" )
-        if choice == 1 then
-            stateName = "config_ld"
-        elseif choice == 2 then
-            stateName = "config_faces"
-        elseif choice ~= nil then
-            print( "INVALID INPUT: " .. choice)
-        end
-
-
     elseif stateName == "config_ld" then
         print( "--             Configure LaunchDarkly            --" )
         print( "---------------------------------------------------" )
@@ -126,6 +118,77 @@ while true do
         print( "7 ) Change flag targeting" )
         print( "---------------------------------------------------" )
         print( "0 ) Go back" )
+    elseif stateName == "config_faces" then
+        print( "--              Configure Interfaces             --" )
+        print( "---------------------------------------------------" )
+        print( "1 ) Front   - (STATUS)" )
+        print( "2 ) Back    - (STATUS)" )
+        print( "3 ) Right   - (STATUS)" )
+        print( "4 ) Left    - (STATUS)" )
+        print( "5 ) Top     - (STATUS)" )
+        print( "6 ) Buttom  - (STATUS)" )
+        print( "7 ) Default - (STATUS)" )
+        print( "---------------------------------------------------" )
+        print( "0 ) Go back" )
+    elseif stateName == "config_face" then
+        print( "--              Configure Interface              --" )
+        print( "---------------------------------------------------" )
+        print( "Side (SIDE) is currently configured to (STATUS).")
+        print( "---------------------------------------------------" )
+        print( "1 ) Disabled                 (Do nothing)" )
+        print( "2 ) Default                  (Use behavior of 'default' face)" )
+        print( "3 ) Output - flag variation  (Output redstone signal)" )
+        print( "4 ) Input  - track goal      (Track a specific goal using redstone signal)" )
+        print( "5 ) Input  - goal metric     (Track a specific metric using redstone signal)" )
+        print( "6 ) Input  - flag state      (Turn flag on/off using redstone signal)" )
+        print( "7 ) Input  - flag targeting  (Turn flag on/off for user using redstone signal)")
+        print( "8 ) Input  - redstone state  (Include redstone signal in user attributes)" )
+        print( "9 ) Input  - redstone count  (Include redstone signal in user attributes)" )
+        print( "---------------------------------------------------" )
+        print( "0 ) Go back" )
+    else
+        print( "INVALID STATE: " .. stateName)
+        skipInput = true
+    end
+
+    choice = nil
+    if skipInput then
+        local event, key = os.pullEvent( "key_down" )
+        if key == keys.one then
+        choice = 1
+        elseif key == keys.two then
+            choice = 2
+        elseif key == keys.three then
+            choice = 3
+        elseif key == keys.four then
+            choice = 4
+        elseif key == keys.five then
+            choice = 5
+        elseif key == keys.six then
+            choice = 6
+        elseif key == keys.seven then
+            choice = 7
+        elseif key == keys.eight then
+            choice = 8
+        elseif key == keys.nine then
+            choice = 9
+        elseif key == keys.zero then
+            choice = 0
+        end
+    end
+    skipInput = false
+
+    if stateName == "main" then
+        if choice == 1 then
+            stateName = "config_ld"
+        elseif choice == 2 then
+            stateName = "config_faces"
+        elseif choice ~= nil then
+            print( "INVALID INPUT: " .. choice)
+        end
+
+
+    elseif stateName == "config_ld" then
         if choice == 1 then
             print("Enter new client-side ID: ")
             state.clientSideID = read()
@@ -145,17 +208,6 @@ while true do
 
 
     elseif stateName == "config_faces" then
-        print( "--              Configure Interfaces             --" )
-        print( "---------------------------------------------------" )
-        print( "1 ) Front   - (STATUS)" )
-        print( "2 ) Back    - (STATUS)" )
-        print( "3 ) Right   - (STATUS)" )
-        print( "4 ) Left    - (STATUS)" )
-        print( "5 ) Top     - (STATUS)" )
-        print( "6 ) Buttom  - (STATUS)" )
-        print( "7 ) Default - (STATUS)" )
-        print( "---------------------------------------------------" )
-        print( "0 ) Go back" )
         if choice == 0 then
             stateName = "main"
         elseif choice ~= nil then
@@ -164,21 +216,6 @@ while true do
 
 
     elseif stateName == "config_face" then
-        print( "--              Configure Interface              --" )
-        print( "---------------------------------------------------" )
-        print( "Side (SIDE) is currently configured to (STATUS).")
-        print( "---------------------------------------------------" )
-        print( "1 ) Disabled                 (Do nothing)" )
-        print( "2 ) Default                  (Use behavior of 'default' face)" )
-        print( "3 ) Output - flag variation  (Output redstone signal)" )
-        print( "4 ) Input  - track goal      (Track a specific goal using redstone signal)" )
-        print( "5 ) Input  - goal metric     (Track a specific metric using redstone signal)" )
-        print( "6 ) Input  - flag state      (Turn flag on/off using redstone signal)" )
-        print( "7 ) Input  - flag targeting  (Turn flag on/off for user using redstone signal)")
-        print( "8 ) Input  - redstone state  (Include redstone signal in user attributes)" )
-        print( "9 ) Input  - redstone count  (Include redstone signal in user attributes)" )
-        print( "---------------------------------------------------" )
-        print( "0 ) Go back" )
         if choice == 0 then
             stateName = "main"
         elseif choice ~= nil then
@@ -187,31 +224,6 @@ while true do
 
 
     else
-        print( "INVALID STATE: " .. stateName)
         stateName="main"
-    end
-
-    choice = nil
-    local event, key = os.pullEvent( "key_down" )
-    if key == keys.one then
-       choice = 1
-    elseif key == keys.two then
-        choice = 2
-    elseif key == keys.three then
-        choice = 3
-    elseif key == keys.four then
-        choice = 4
-    elseif key == keys.five then
-        choice = 5
-    elseif key == keys.six then
-        choice = 6
-    elseif key == keys.seven then
-        choice = 7
-    elseif key == keys.eight then
-        choice = 8
-    elseif key == keys.nine then
-        choice = 9
-    elseif key == keys.zero then
-        choice = 0
     end
 end
